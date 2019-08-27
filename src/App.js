972b4b0745442ecd4c53fc7301540b0cdcf18cd0
jeevan <jeevangkr37@gmail.com>
2019-08-28 00:23:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import * as datal from './example.json';
+//import { stat } from 'fs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(){
+    super() 
+      this.state = {
+        data: datal
+      }
+    
+  }
+
+  
+  render(){return (
+    <div>
+      <h1>
+        {
+          this.state.data.map(function(people){
+             return (<div>{people.A}</div>);
+          })
+        }
+      </h1>
     </div>
   );
-}
+}}
 
 export default App;
